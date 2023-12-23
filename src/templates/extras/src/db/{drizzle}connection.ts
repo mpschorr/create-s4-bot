d6303af6e32904f6378ec439a669ce61a94d5a59
mpschorr <real.jeel@gmail.com>
2023-12-23 13:12:48 -0500

    fix: drizzle schema types

--- src/templates/extras/src/db/{drizzle}connection.ts
+++ src/templates/extras/src/db/{drizzle}connection.ts
@@ -2,13 +2,15 @@ import { container } from '@sapphire/framework';
 import Database from 'better-sqlite3';
 import { BetterSQLite3Database, drizzle } from 'drizzle-orm/better-sqlite3';
 
+import * as schema from './schema';
+
 const sqlite = new Database('sqlite.db');
-export const db = drizzle(sqlite);
+const db = drizzle(sqlite, { schema: schema });
 
 // Inject into container with sapphire (https://www.sapphirejs.dev/docs/Guide/additional-information/using-and-extending-container)
 declare module '@sapphire/pieces' {
-  interface Container {
-    db: BetterSQLite3Database;
-  }
+	interface Container {
+		db: BetterSQLite3Database<typeof schema>;
+	}
 }
 container.db = db;
--- src/templates/extras/src/db/{drizzle}schema.ts
+++ src/templates/extras/src/db/{drizzle}schema.ts
@@ -1,3 +1,4 @@
 // import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 // TODO Define your schema here!
+// https://orm.drizzle.team/docs/sql-schema-declaration
