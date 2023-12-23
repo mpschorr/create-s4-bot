54de49f92ef3f500271d0d7cc480cf2ebc241235
mpschorr <real.jeel@gmail.com>
2023-12-23 13:29:19 -0500

    fix: db was not exported for migrations

--- src/templates/extras/src/db/{drizzle}connection.ts
+++ src/templates/extras/src/db/{drizzle}connection.ts
@@ -5,7 +5,7 @@ import { BetterSQLite3Database, drizzle } from 'drizzle-orm/better-sqlite3';
 import * as schema from './schema';
 
 const sqlite = new Database('sqlite.db');
-const db = drizzle(sqlite, { schema: schema });
+export const db = drizzle(sqlite, { schema: schema });
 
 // Inject into container with sapphire (https://www.sapphirejs.dev/docs/Guide/additional-information/using-and-extending-container)
 declare module '@sapphire/pieces' {
